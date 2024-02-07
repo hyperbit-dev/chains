6745a1da5efb6728a075b1b0652443d24fb96ee1
NeuraiProject <122438095+NeuraiProject@users.noreply.github.com>
2024-02-07 13:19:09 +0100

    Update testnet.ts
    Add 1 to BIP44 in Testnet

--- src/chains/neurai/testnet.ts
+++ src/chains/neurai/testnet.ts
@@ -23,7 +23,7 @@ export const testnet: TestNet = {
       private: 0x04358394,
       public: 0x043587cf,
     },
-    bip44: 0,
+    bip44: 1,
     // https://github.com/NeuraiProject/Neurai/blob/main/src/chainparams.cpp#L423
     private: 0xef,
     // https://github.com/NeuraiProject/Neurai/blob/main/src/chainparams.cpp#L359
