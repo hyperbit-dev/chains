5dce20d2aa5cdb1ae6210a53573fed3bc9d9e02e
Michael <mmcshinsky@gmail.com>
2025-02-11 16:51:49 -0700

    feat: add chain symbols as a supported type
    v1.9.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as _chains from './chains';
-import { Networks } from './types/base';
+import { Networks, Symbol } from './types/base';
 
-export const chains: Record<string, Networks> = _chains;
+export const chains: Record<Symbol, Networks> = _chains;
 export * from './chains';
 export * from './utils';
 export * from './types';
